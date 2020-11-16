8b49edb07314a72963ac5788a36b9918ba8115a5
Petr <superdytko@gmail.com>
2020-11-16 20:30:49 +0300

    First check calculator

--- homework.py
+++ homework.py
@@ -1,9 +1,80 @@
 import datetime as dt
 
 
-class Calculator:
-    pass
-
-
 class Record:
-    pass
+    date_format = '%d.%m.%Y'
+
+    def __init__(self, amount, comment, date=None):
+        self.amount = amount
+        self.comment = comment
+        self.date = dt.datetime.strptime(date, self.date_format).date() if date else dt.datetime.now().date()
+
+    def __str__(self):
+        return f'{self.amount}'
+
+
+class Calculator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
+
+    def add_record(self, new_record):
+        self.records.append(new_record)
+
+    def get_today_status(self):
+        today_amount = 0
+        now = dt.datetime.now()
+        for i in self.records:
+            if i.date == now.date():
+                today_amount += i.amount
+        return today_amount
+
+    def __str__(self):
+        return ', '.join(str(s) for s in self.records)
+
+
+class CaloriesCalculator(Calculator):
+
+    # "Get 7-days status count"
+    def get_week_status(self):
+        sum_amount = 0
+        today = dt.datetime.now()
+        delta = dt.timedelta(days=7)
+        start_date = today - delta
+        for i in self.records:
+            if start_date < i.date <= today:
+                sum_amount += i.amount
+        return sum_amount
+
+    def get_calories_remained(self):
+        today_calories = super().get_today_status()
+        today_calories_left = self.limit - today_calories
+        if today_calories_left > 0:
+            return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {today_calories_left} кКал»'
+        else:
+            return 'Хватит есть!'
+
+
+class CashCalculator(Calculator):
+
+    def __str__(self):
+        return f'{self.limit}'
+
+    def get_today_cash_remained(self, currency):
+        EURO_RATE = 90.0
+        USD_RATE = 77.0
+        currencies = {
+            'rub': [1, 'руб'],
+            'usd': [USD_RATE, 'usd'],
+            'eur': [EURO_RATE, 'euro']
+        }
+        today_cash_spend = super().get_today_status()
+        today_cash_left = self.limit - today_cash_spend
+        rate = currencies[currency]
+        money = round(today_cash_left / rate[0], 2)
+        if money > 0:
+            return f'На сегодня осталось {money}, {rate[1]}'
+        elif money == 0:
+            return 'Денег нет, держись'
+        else:
+            return f'Денег нет, держись: твой долг -{abs(money)}, {rate[1]}'
